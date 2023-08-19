059830d448d9aee0455e25b758f24ea05a4f0d37
Egao <gershommethod02@gmail.com>
2023-08-19 11:20:36 -0700

    task 0 updated

--- main.h
+++ main.h
@@ -23,15 +23,20 @@ typedef struct var
 	int y;
 } var;
 
-#define INC(format, y) do {format++; y++;} while (0)
-#define VAR(par, b) do {va_list par; var b;} while (0)
+#define INC(format, y) \
+	do {format++; \
+		y++; }  \
+		while (0)
+/*#define VAR(par, b) do {va_list par; var b;} while (0)*/
 #define VALUES(v) do { \
-	v.x = 0; \
-	v.y = 0; \
+	(v.x) = 0; \
+	(v.y) = 0; \
 } while (0)
 #define CHECK_FORMAT(format) \
+{ \
 	if ((format) == NULL) \
-	return 0
+	return (0) \
+}
 int _putchar(char q);
 void printstr(char *s);
 int _printf(const char *format, ...);
