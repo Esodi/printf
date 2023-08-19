649e4307bdb6ac4edf8e19c23e7bd65114b9dcdc
Egao <gershommethod02@gmail.com>
2023-08-19 11:04:26 -0700

    task 0 added (printf project)

--- main.h
+++ main.h
@@ -6,6 +6,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct var - contains variables
+ * @c: char var
+ * @p: char var
+ * @s: string
+ * @x: int var
+ * @y: int var
+ */
 typedef struct var
 {
 	char c;
@@ -15,8 +23,8 @@ typedef struct var
 	int y;
 } var;
 
-#define INC(format, y) do {format++; y++;} while(0)
-#define VAR(par, b) do {va_list par; var b;} while(0)
+#define INC(format, y) do {format++; y++;} while (0)
+#define VAR(par, b) do {va_list par; var b;} while (0)
 #define VALUES(v) do { \
 	v.x = 0; \
 	v.y = 0; \
@@ -26,5 +34,6 @@ typedef struct var
 	return 0
 int _putchar(char q);
 void printstr(char *s);
+int _printf(const char *format, ...);
 
 #endif /*MAIN.H*/
