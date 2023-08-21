75e0516b49cfc13704ff12b2109bc54d7c606d82
Egao <gershommethod02@gmail.com>
2023-08-21 05:50:55 -0700

    Task 0 update

--- tag_func.c
+++ tag_func.c
@@ -2,7 +2,7 @@
 
 static int tag_caller(char converter, va_list args);
 static int prefix_handler(char **string);
-static int tag_invalid();
+static int tag_invalid(char converter);
 static char converter_handler(char **string);
 
 int tag_handler(char **string, va_list args)
@@ -30,6 +30,7 @@ static char converter_handler(char **string)
 
 static int tag_caller(char converter, va_list args)
 {
+
 	if (converter == 'c')
 		return (char_handler(args));
 	if (converter == 's')
@@ -44,8 +45,10 @@ static int tag_caller(char converter, va_list args)
 		return (hex_handler(converter, args));*/
 	if (converter == '%')
 		return (percent_handler());
+	if (converter == '\0')
+		return (-1);
 	else
-		return (tag_invalid());
+		return (tag_invalid(converter));
 }
 
 static int prefix_handler(char **string)
@@ -73,12 +76,9 @@ static int prefix_handler(char **string)
 	return (counter);
 }
 
-/*static int tag_invalid(char converter)*/
-static int tag_invalid()
+static int tag_invalid(char converter)
 {
-/*	_putchar_fd('%', 1);
+	_putchar_fd('%', 1);
 	_putchar_fd(converter, 1);
-	return (2);*/
-	return (-1);
-
+	return (2);
 }
